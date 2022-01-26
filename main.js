f11e8b91bfcffa12f67d765f49711eb594f28b63
Karan Vyas <karanvyas2907@gmail.com>
2022-01-26 14:20:30 +0530

    added html, css and fetched the api in js

---
+++ main.js
@@ -0,0 +1,34 @@
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let key = `67521aab479b5104a4a9037f587854c6`;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  let temperatureDegree = document.querySelector(".temperature-degree");
+  let locationTimezone=document.querySelector(".location-timezone");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${key}`;
+      // console.log(api);
+      fetch(api)
+        .then((Response) => {
+          return Response.json();
+        })
+        .then((data) => {
+          console.log(data.current);
+          const {timezone}= data;
+          console.log(timezone);
+          const { temp } = data.current;
+          console.log(temp);
+          const {description}=data.current.weather[0];
+          console.log(description);
+
+        //   set DOM elements from the API 
+        });
+    });
+  }
+});
